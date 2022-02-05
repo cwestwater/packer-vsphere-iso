75669acd381c0b98c264215ca425ef573f8005fa
Colin Westwater <cwestwater@gmail.com>
2022-02-05 19:30:39 +0000

    Fix to accomodate Core vs Desktop Exp builds

--- scripts/win2016/remove-features.ps1
+++ scripts/win2016/remove-features.ps1
@@ -2,32 +2,49 @@
 
 $ErrorActionPreference = "Stop"
 
-# Remove XPS Viewer
+# Determine if Core or Desktop Experience
+$osVersion = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name InstallationType
+
+# Common Features
+
+# Remove SMB 1.0/CIFS File Sharing Support
 Write-Host "Remove SMB 1.0/CIFS File Sharing Support"
-Uninstall-WindowsFeature -Name FS-SMB1
+Uninstall-WindowsFeature -Name FS-SMB1 | Out-Null
 
 # Remove PowerShell v2
 Write-Host "Remove PowerShell v2"
-Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -NoRestart
-
-# Remove Windows Media Player
-Write-Host "Remove Windows Media Player"
-Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart
-
-# Remove Windows Media Playback
-Write-Host "Remove Windows Media Playback"
-Disable-WindowsOptionalFeature -Online -FeatureName MediaPlayback -NoRestart
-
-# Remove Internet Explorer 11
-Write-Host "Remove Internet Explorer 11"
-Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64 -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -NoRestart | Out-Null
 
 # Remove Microsoft Print to PDF
 Write-Host "Remove Microsoft Print to PDF"
-Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Printing-PrintToPDFServices-Package -NoRestart
-Disable-WindowsOptionalFeature -Online -FeatureName Printing-PrintToPDFServices-Features -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName Printing-PrintToPDFServices-Features -NoRestart | Out-Null
 
 # Remove Microsoft XPS Document Writer
 Write-Host "Remove Microsoft XPS Document Writer"
-Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Printing-XPSServices-Package -NoRestart
-Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart | Out-Null
+
+# If Desktop Experience is installed disable these services, otherwise Exit cleanly
+
+if ( $osVersion -eq "Server" )
+{
+    # Remove XPS Viewer
+    Write-Host "Remove XPS Viewer"
+    Uninstall-WindowsFeature -Name XPS-Viewer | Out-Null
+
+    # Remove Windows Media Player
+    Write-Host "Remove Windows Media Player"
+    Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart | Out-Null
+
+    # Remove Windows Media Playback
+    Write-Host "Remove Windows Media Playback"
+    Disable-WindowsOptionalFeature -Online -FeatureName MediaPlayback -NoRestart | Out-Null
+
+    # Remove Internet Explorer 11
+    Write-Host "Remove Internet Explorer 11"
+    Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64 -NoRestart | Out-Null
+
+}
+else
+{
+    exit 0
+}
--- scripts/win2019/remove-features.ps1
+++ scripts/win2019/remove-features.ps1
@@ -2,30 +2,44 @@
 
 $ErrorActionPreference = "Stop"
 
-# Remove XPS Viewer
-Write-Host "Remove XPS Viewer"
-Uninstall-WindowsFeature -Name XPS-Viewer
+# Determine if Core or Desktop Experience
+$osVersion = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name InstallationType
 
-# Remove Microsoft XPS Document Writer
-Write-Host "Remove Microsoft XPS Document Writer"
-Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart
+# Common Features
 
 # Remove PowerShell v2
 Write-Host "Remove PowerShell v2"
-Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -NoRestart
+Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -NoRestart | Out-Null
 
-# Remove Windows Media Player
-Write-Host "Remove Windows Media Player"
-Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart
+# If Desktop Experience is installed disable these services, otherwise Exit cleanly
 
-# Remove Windows Media Playback
-Write-Host "Remove Windows Media Playback"
-Disable-WindowsOptionalFeature -Online -FeatureName MediaPlayback -NoRestart
+if ( $osVersion -eq "Server" )
+{
+    # Remove XPS Viewer
+    Write-Host "Remove XPS Viewer"
+    Uninstall-WindowsFeature -Name XPS-Viewer | Out-Null
 
-# Remove Internet Explorer 11
-Write-Host "Remove Internet Explorer 11"
-Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64 -NoRestart
+    # Remove Microsoft XPS Document Writer
+    Write-Host "Remove Microsoft XPS Document Writer"
+    Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart | Out-Null
 
-# Remove Microsoft Print to PDF
-Write-Host "Remove Microsoft Print to PDF"
-Disable-WindowsOptionalFeature -Online -FeatureName Printing-PrintToPDFServices-Features -NoRestart
+    # Remove Windows Media Player
+    Write-Host "Remove Windows Media Player"
+    Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart | Out-Null
+
+    # Remove Windows Media Playback
+    Write-Host "Remove Windows Media Playback"
+    Disable-WindowsOptionalFeature -Online -FeatureName MediaPlayback -NoRestart | Out-Null
+
+    # Remove Internet Explorer 11
+    Write-Host "Remove Internet Explorer 11"
+    Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64 -NoRestart | Out-Null
+
+    # Remove Microsoft Print to PDF
+    Write-Host "Remove Microsoft Print to PDF"
+    Disable-WindowsOptionalFeature -Online -FeatureName Printing-PrintToPDFServices-Features -NoRestart | Out-Null
+}
+else
+{
+    exit 0
+}
