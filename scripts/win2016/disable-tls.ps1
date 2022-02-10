e2949181f45e5b5ec4f34735cadda97b3fe25a49
Colin Westwater <cwestwater@gmail.com>
2022-02-10 08:22:33 +0000

    Fix formatting

--- scripts/win2016/disable-tls.ps1
+++ scripts/win2016/disable-tls.ps1
@@ -14,7 +14,6 @@ New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders
 
 # Disable TLS 1.1
 Write-Host "Disable TLS 1.1"
-
 New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1" | Out-Null
 New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server" | Out-Null
 New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client" | Out-Null
